7de91ffe9c690747921e81b0cf3f685c6faf998f
Julian Aureliano Cienfuegos <melvyniandrag@gmail.com>
2015-09-14 16:54:27 -0400

    added a translator

--- chemlhi/translate_to_bio.py
+++ chemlhi/translate_to_bio.py
@@ -1,3 +1,10 @@
+import sys
+
+""" 
+This script is used for changing the format of a chem lhi file to the bio format. 
+Then the bio tool can be used to update dependencies, and then the file can be translated back.
+"""
+
 # ---------------------------------------------------------------------------
 # helper funcs
 # ---------------------------------------------------------------------------
@@ -22,7 +29,7 @@ def make_new_row(split_row):
 			if '-' in item:
 				bds = [int(val) for val in item.split('-')]
 				split_list[idx] = ','.join([str(i) for i in range(bds[0], bds[1] + 1)])
-	print split_bars
+	#print split_bars
 	no_hyphen_yet = [','.join(item) for  item in split_bars]
 	return '-'.join(no_hyphen_yet)
 
@@ -46,12 +53,57 @@ def translate_c2b(row):
 	new_row = make_new_row(split_row)
 	return levels, new_row + ' ' + dist
 
+def add_bars(r, i = 0):
+	""" Do you know what tail recursion is? Here's an example! """
+	if i < len(r) - 1:
+		if type(r[i]) == int and r[i+1] == r[i] + 1:
+			return add_bars(r, i+1)
+		else:
+			r.insert(i+1, '|')
+			return add_bars(r, i+2)
+	else:
+		return r
+
+
+def remove_intermediate(row):
+	bar_idxs = [-1] + [idx for idx, item in enumerate(row) if item == '|']
+	for i in range(len(bar_idxs)):
+		if i+1 != len(bar_idxs): # Then we aren't at the last index.
+			if bar_idxs[i+1] > bar_idxs[i] + 1: # Then there are at least two numbers in this interval and we need a hyphen.
+				row[bar_idxs[i] : bar_idxs[i+1]] = [row[bar_idxs[i], '-', row[bar_idxs[i+1] - 1]]
+				bar_idxs = [-1] + [idx for idx, item in enumerate(row) if item == '|']
+		else:
+			if len(row) > bar_idxs[i] + 1: # Then there are at least two numbers in this interval and we need a hyphen.
+				row[bar_idxs[i] : ] = [row[bar_idxs[i], '-', row[-1]]
+	return row
+
+def interleave(levels, row):
+	pass
+
 def translate_b2c(levels , row):
 	'''
 	input: a row in biolhi format and the associated levels
 	output: the same row reformatted in the chemlhi format.
 	'''
-	pass
+	# Take in a row and a list of levels associated with the row.
+	# peel off and store the distribution information.
+	# Translate the values between the hyphens into chem format. 
+	# Then join the level numbers and the numbers between the hypens into one dependency string.
+	dist = row.split(' ')[1]
+	row = row.split(' ')[0]
+	row = row.split('-')
+	for i, r in enumerate(row):
+		row[i] = [int(j) for j in r.split(',')]
+	print row
+	for i, r in enumerate(row):
+		row[i] = add_bars(r)
+	for i, r in enumerate(row):
+		row[i] = remove_intermediates(r)
+	return row
+	chemrow = interleave(levels, row)
+	return chemrow + ' ' + dist
 
 if __name__ == '__main__':
 	print translate_c2b('[1(2|4-9)&2(3-9)] a_dist')
+	#print add_bars('1,2,3,4,6')
+	print translate_b2c(['1', '2'], '1,2,3,5-4,6,7,9 dist')
